191b0c932d52ca4722330a53c0b1518d7f9a1e45
Dalton Pearson <dpearson@niagaracollege.ca>
2018-10-05 11:19:11 -0400

    misc

---
+++ dpearson4_MVC_Music/Data/MusicMigrations/20181004153810_UniqueTitleYear.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dpearson4_MVC_Music.Data.MusicMigrations
+{
+    public partial class UniqueTitleYear : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Albums_YearProduced_Name",
+                schema: "MUSIC",
+                table: "Albums",
+                columns: new[] { "YearProduced", "Name" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Albums_YearProduced_Name",
+                schema: "MUSIC",
+                table: "Albums");
+        }
+    }
+}
