86fbc811d30f57aee5127e7111a9534be6b2bc16
Dalton Pearson <dpearson@niagaracollege.ca>
2018-10-05 08:46:57 -0400

    initial commit

---
+++ solution_MVC_Music/Controllers/GenresController.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using solution_MVC_Music.Data;
+using solution_MVC_Music.Models;
+
+namespace solution_MVC_Music.Controllers
+{
+    public class GenresController : Controller
+    {
+        private readonly MusicContext _context;
+
+        public GenresController(MusicContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Genres
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Genres.ToListAsync());
+        }
+
+        // GET: Genres/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var genre = await _context.Genres
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return View(genre);
+        }
+
+        // GET: Genres/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Genres/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name")] Genre genre)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(genre);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to add genre. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return View(genre);
+        }
+
+        // GET: Genres/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var genre = await _context.Genres.FindAsync(id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+            return View(genre);
+        }
+
+        // POST: Genres/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name")] Genre genre)
+        {
+            if (id != genre.ID)
+            {
+                return NotFound();
+            }
+
+            var genreToUpdate = await _context.Genres.SingleOrDefaultAsync(s => s.ID == id);
+
+            if (await TryUpdateModelAsync<Genre>(
+        genreToUpdate,
+        "",
+        g => g.Name))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException)
+                {
+                    {
+                        //Log the error (uncomment ex variable name and write a log.)
+                        ModelState.AddModelError("", "Unable to save changes. " +
+                            "Try again, and if the problem persists, " +
+                            "see your system administrator.");
+                    }
+                }
+            }
+            return View(genre);
+        }
+
+        // GET: Genres/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var genre = await _context.Genres
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return View(genre);
+        }
+
+        // POST: Genres/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (GenreHasSongs(id))
+            {
+                ModelState.AddModelError("", "Unable to delete genre. Genre is assigned to song.");
+                return RedirectToAction(nameof(Index));
+            }
+
+            if (GenreHasAlbums(id))
+            {
+                ModelState.AddModelError("", "Unable to delete genre. Genre is assigned to album.");
+                return RedirectToAction(nameof(Index));
+            }
+
+            var genre = await _context.Genres.FindAsync(id);
+
+            if (genre == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            try { 
+                _context.Genres.Remove(genre);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException)
+            {
+                return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
+            }
+        }
+
+        private bool GenreExists(int id)
+        {
+            return _context.Genres.Any(e => e.ID == id);
+        }
+
+        private bool GenreHasSongs(int id)
+        {
+            return _context.Songs.Any(e => e.GenreID == id);
+        }
+
+        private bool GenreHasAlbums(int id)
+        {
+            return _context.Albums.Any(e => e.GenreID == id);
+        }
+    }
+}
---
+++ solution_MVC_Music/Data/MusicMigrations/20181004153810_UniqueTitleYear.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace solution_MVC_Music.Data.MusicMigrations
+{
+    public partial class UniqueTitleYear : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Albums_YearProduced_Name",
+                schema: "MUSIC",
+                table: "Albums",
+                columns: new[] { "YearProduced", "Name" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Albums_YearProduced_Name",
+                schema: "MUSIC",
+                table: "Albums");
+        }
+    }
+}
